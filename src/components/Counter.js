56c1ab0ef9a38da657737f22b52aa32b169d1ea5
Dominic Farnham <dominic.farnham@hotmail.co.uk>
2019-01-12 12:01:30 +0000

    Converted Counter class component into a functional component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,28 +1,25 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class Counter extends Component {
-
-  incrementScore = () => {
+const Counter = (props) => {
+  const incrementScore = () => {
     this.setState(prevState => ({
       score: prevState.score + 1
-    }));
+    }))
   }
 
-  decrementScore = () => {
+  const decrementScore = () => {
     this.setState(prevState => ({
       score: prevState.score - 1
-    }));
+    }))
   }
 
-  render() {
-    return (
-      <div className="counter">
-        <button className="counter-action decrement"> - </button>
-        <span className="counter-score">{this.props.score}</span>
-        <button className="counter-action increment"> + </button>
-      </div>
-    );
-  }
+  return (
+    <div className='counter'>
+      <button className='counter-action decrement'> - </button>
+      <span className='counter-score'>{props.score}</span>
+      <button className='counter-action increment'> + </button>
+    </div>
+  )
 }
 
 export default Counter
